025603af209ffe2df160ea9c6c56ca232102555e
Micaela Colpi <micaelacolpi@gmail.com>
2021-07-07 09:33:33 -0500

    second commit

---
+++ CoffeeShop/Models/Coffee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShop.Models
+{
+    public class Coffee: BeanVariety
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int BeanVarietyId { get; set; }
+    }
+}
